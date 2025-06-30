7803086e067043789f4c07db0fbba8c620b3b3f1
Toyse_Devs <olayonwatoyib05@gmail.com>
2025-06-30 21:41:45 +0100

    The carousel

--- website/index.js
+++ website/index.js
@@ -6,3 +6,87 @@ window.addEventListener('scroll', function() {
     navbar.classList.remove('scrolled');
   }
 });
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+
+// Scroll Animation
+const testimonials = document.querySelectorAll(".testimonial");
+
+const testimonialObserve = new IntersectionObserver((enter) => {
+  enter.forEach(entry => {
+    entry.target.classList.toggle("show", entry.isIntersecting);
+  });
+}, {
+  threshold: 0.5
+});
+
+testimonials.forEach(testimonial => testimonialObserve.observe(testimonial));
+
+// Carousel
+const cards = document.querySelectorAll(".testimonial-card");
+const nextBtn = document.querySelector("#next");
+const prevBtn = document.querySelector("#prev");
+const carousel = document.querySelector("#carousel");
+
+let currentIndex = 0;
+let autoSlide;
+let hovered = false;
+
+// Show only the current testimonial
+function showSlide(index) {
+  cards.forEach((card, each) => {
+    card.classList.remove("active");
+    if (each === index) card.classList.add("active");
+  });
+}
+
+// Next slide
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % cards.length;
+  showSlide(currentIndex);
+}
+
+// Previous slide
+function prevSlide() {
+  currentIndex = (currentIndex - 1 + cards.length) %  cards.length;
+  showSlide(currentIndex);
+}
+
+// Start auto-slide
+function startAutoSlide() {
+  autoSlide = setInterval(() => {
+    if (!hovered) nextSlide();
+  }, 5000);
+}
+
+// Stop auto-slide
+function stopAutoSlide() {
+  clearInterval(autoSlide);
+}
+
+// Handle hove to pause auto-slide
+carousel.addEventListener("mouseenter", () => {
+  hovered = true;
+});
+
+carousel.addEventListener("mouseleave", () => {
+  hovered = false;
+});
+
+// Button click event
+nextBtn.addEventListener("click", () => {
+  nextSlide();
+});
+
+prevBtn.addEventListener("click", () => {
+  prevBtn();
+});
+
+// Initialize
+showSlide(currentIndex);
+startAutoSlide();
