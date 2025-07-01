42013b7d20621affea54d0087c3d5ebeaa60c568
Toyse_Devs <olayonwatoyib05@gmail.com>
2025-07-01 09:59:09 +0100

    The title, and the testimonial responsiveness

--- index.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-  const navbar = document.querySelector('.navbar');
-  if (window.scrollY > 50) { // Adjust threshold (50px)
-    navbar.classList.add('scrolled');
-  } else {
-    navbar.classList.remove('scrolled');
-  }
-});
